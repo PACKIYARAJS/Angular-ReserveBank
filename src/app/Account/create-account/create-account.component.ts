2482d690887b5e840287573bddb1d6ffb5916eac
PACKIYARAJS <packiyarajs350@gmail.com>
2025-01-27 12:25:25 +0530

    Create-Account

--- src/app/Account/create-account/create-account.component.ts
+++ src/app/Account/create-account/create-account.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+        AccountName = 'HDFC';
+        bankName ='';
+        headOffice='';
+        NoOfBranches=0;
+        TotalBalance=0;
+        ExpectedGrowthRate=0;
+        NoOfCustomers = 0;
 
+  RegisterBank(){
+
+    let RequestBody= {
+      bankName : this.bankName,
+      headOffice : this.headOffice,
+      NoOfBranches : this.NoOfBranches,
+      TotalBalance : this.TotalBalance,
+      ExpectedGrowthRate : this.ExpectedGrowthRate,
+      NoOfCustomers : this.NoOfCustomers,
+    }
+    console.log(RequestBody);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,23 @@ import { RouterOutlet } from '@angular/router';
 import { CreateAccountComponent } from './Account/create-account/create-account.component';
 import { AdminAccountComponent } from './Account/admin-account/admin-account.component';
 import { AdminRupeeComponent } from './Rupee/admin-rupee/admin-rupee.component';
+import { ViewAccountComponent } from './Account/view-account/view-account.component';
+import { CloseAccountComponent } from './Account/close-account/close-account.component';
+import { AdminRuleComponent } from './BankRules/admin-rule/admin-rule.component';
+import { DefineRuleComponent } from './BankRules/define-rule/define-rule.component';
+import { ApplyLoanComponent } from './Loan/apply-loan/apply-loan.component';
+import { LoanAdminComponent } from './Loan/loan-admin/loan-admin.component';
+import { PayLoanComponent } from './Loan/pay-loan/pay-loan.component';
+import { ViewLoanComponent } from './Loan/view-loan/view-loan.component';
+import { RupeeRequestComponent } from './Rupee/rupee-request/rupee-request.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CreateAccountComponent , AdminAccountComponent, AdminRupeeComponent],
+  imports: [CommonModule, RouterOutlet, CreateAccountComponent , AdminAccountComponent, CloseAccountComponent, ViewAccountComponent, 
+            AdminRuleComponent, DefineRuleComponent, 
+            ApplyLoanComponent, LoanAdminComponent, PayLoanComponent, ViewLoanComponent, 
+            AdminRupeeComponent, RupeeRequestComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
