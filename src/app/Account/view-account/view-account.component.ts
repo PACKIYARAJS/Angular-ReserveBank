4462732b06b0fb3ea6e3124ba5c3fbb9e0b8ba47
PACKIYARAJS <packiyarajs350@gmail.com>
2025-01-29 12:36:42 +0530

    API

--- src/app/Account/view-account/view-account.component.ts
+++ src/app/Account/view-account/view-account.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../Services/api.service';
 
 @Component({
   selector: 'app-view-account',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-account.component.html',
   styleUrl: './view-account.component.scss'
 })
-export class ViewAccountComponent {
+export class ViewAccountComponent implements OnInit{
+
+  constructor(private api : ApiService){}
+
+  ngOnInit(): void {
+    this.gatDataValue();
+  }
+
+  accountData:any;
+
+  gatDataValue(){
+
+    this.api.getData().subscribe(
+
+      (responseData:any)=>{
+
+        this.accountData = responseData;
+
+      },
+
+      err=>{console.log(err)}
+
+    );
+
+  }
 
 }
